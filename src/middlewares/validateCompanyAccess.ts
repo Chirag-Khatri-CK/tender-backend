a871465dfe3b54f244c61ce842ce63565539d12c
Chirag Khatri <chirag.khatri@webneed.com>
2026-04-28 02:01:56 +0530

    changes

--- src/middlewares/validateCompanyAccess.ts
+++ src/middlewares/validateCompanyAccess.ts
@@ -5,27 +5,22 @@ import Company from "../models/company/Company";
 import { AppError } from "../utils/AppError";
 
 export const validateCompanyAccess = async (
-    req: Request & { user?: { userId: string; role: string }; company?: any },
+    req: Request & { user?: any; company?: any },
     res: Response,
     next: NextFunction
 ) => {
     try {
         const companyId = String(req.params.companyId || req.params.id);
-        const userId = req.user?.userId;
+        const userId = req.user._id;
         const role = req.user?.role;
 
-        if (!Types.ObjectId.isValid(companyId))
-            throw new AppError(400, "Invalid Company ID");
+        if (!Types.ObjectId.isValid(companyId)) throw new AppError(400, "Invalid Company ID");
 
-        const company = await Company.findOne({
-            _id: companyId,
-            isDeleted: false,
-        });
+        const company = await Company.findOne({ _id: companyId, isDeleted: false, });
 
         if (!company) throw new AppError(404, "Company not found");
 
-        if (role !== "admin" && company.createdBy.toString() !== userId)
-            throw new AppError(403, "Unauthorized");
+        if (role !== "admin" && company.createdBy.toString() !== userId?.toString()) throw new AppError(403, "Unauthorized");
 
         req.company = company;
         next();
